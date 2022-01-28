2c7d5c082cd790050f0878da63a52a54ab3b9f39
Christopher <ksysiupeel@gmail.com>
2022-01-28 18:14:02 +0100

    Add sessions, Modify .gitignore, config

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from datetime import timedelta
 from .routes.index import index
 from .utils.error import page_not_found
+from config import SECRET_KEY
 
 def create_app():
     # create an instance
@@ -9,6 +10,12 @@ def create_app():
 
     # set blueprints
     app.register_blueprint(index)
+
+    # set secret key
+    app.secret_key = SECRET_KEY
+
+    # set session time
+    app.permanent_session_lifetime = timedelta(minutes=5)
 
     # set error handler
     app.register_error_handler(404, page_not_found)
--- app/routes/index.py
+++ app/routes/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, jsonify, request
+from flask import Blueprint, redirect, jsonify, request, url_for, session
 from config import DISCORD_LOGIN_URL
 from ..utils.oauth2 import Oauth2
 
@@ -11,15 +11,27 @@ def home():
     return jsonify({"msg": "Welcome!"})
 
 
-@index.route("/user")
-def get_user_info():
+@index.route("/login")
+def redirect_to_discord_url():
+    if "user" in session:
+        return redirect(url_for("index.user_data"))
     return redirect(DISCORD_LOGIN_URL)
 
 
 @index.route("/identify", methods=["GET"])
 def identy():
+    if "user" in session:
+        return redirect(url_for("index.user_data"))
+
     code = request.args.get("code")
     token = Oauth2.get_token(code)
-    user_data = Oauth2.get_user(token)
+    data = Oauth2.get_user(token)
+    session["user"] = data
+    return redirect(url_for("index.user_data"))
 
-    return user_data
+
+@index.route("/user", methods=["GET"])
+def user_data():
+    if "user" not in session:
+        return redirect(url_for("index.home"))
+    return session["user"]
